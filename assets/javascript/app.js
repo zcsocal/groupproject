c0172d3319a0d99c9aaa9863d3ff7143bcec467e
abbyreyna <reyna.abby08@gmail.com>
2019-03-16 20:56:03 -0700

    adjustments!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,29 +22,34 @@ lngUser = position.coords.longitude;
     
 var queryURL = "https://www.hikingproject.com/data/get-trails?lat=" + latUser +"&lon=" + lngUser + "&maxDistance=10&key=200430269-7625402c2fab0719cfcddb27c1c9a81c";
 console.log(queryURL);
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response){
+    console.log(response);
+  })
 }
 
+
     // The below code fills in the first row of the table
-     
-     
-     $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      // Create a new table row element
-      var tRow = $("<tr>");
-      console.log(response);
-      // Methods run on jQuery selectors return the selector they we run on
-      // This is why we can create and save a reference to a td in the same statement we update its text
-      var titleTd = $("<td>").text(response.trails[0].name);
-      var summaryTD= $("<td>").text(response.trails[0].summary);
+
+    // }).then(function(response) {
+    //   // Create a new table row element
+    //   var tRow = $("<tr>");
+    //   console.log(response);
+
+    //   // Methods run on jQuery selectors return the selector they we run on
+    //   // This is why we can create and save a reference to a td in the same statement we update its text
+    //   var titleTd = $("<td>").text(response.trails[0].name);
+    //   var summaryTD= $("<td>").text(response.trails[0].summary);
     
         
-      // Append the newly created table data to the table row
-      tRow.append(titleTd);
-      // Append the table row to the table body
-      $("tbody").append(tRow);
-    });
+    //   // Append the newly created table data to the table row
+    //   tRow.append(titleTd);
+    //   // Append the table row to the table body
+    //   $("tbody").append(tRow);
+    // });
 
 
 
